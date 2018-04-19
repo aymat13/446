a55a1770f0066562b9f6a063f307def651a3b06b
aymat13 <32795193+aymat13@users.noreply.github.com>
2018-04-19 11:26:00 +0300

    kek

--- Lab/Lab3/lab3/imem.v
+++ Lab/Lab3/lab3/imem.v
@@ -3,7 +3,7 @@ module imem (input wire [31:0] adr,
 	reg [31:0] RAM[63:0];
 	
 	initial begin
-		$readmemh("C:/Users/AhmetSalih/Documents/GitHub/446/Lab/Lab3/lab3/program.txt",RAM);
+		$readmemh("C:/Users/aymat/Documents/GitHub/446/Lab/Lab3/lab3/program.txt",RAM);
 	end
 	assign inst = RAM[adr[31:2]];
 endmodule
